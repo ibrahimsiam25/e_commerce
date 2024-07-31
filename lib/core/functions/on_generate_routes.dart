5b2a3c3f8fb74f8c692fc756e73b0cc473fb29e3
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-07-31 05:29:45 +0300

    create on boarding

--- lib/core/functions/on_generate_routes.dart
+++ lib/core/functions/on_generate_routes.dart
@@ -1,10 +1,13 @@
-import 'package:e_commerce/features/presention/splash_view.dart';
+import 'package:e_commerce/features/on_boarding/presention/views/on_boarding_View.dart';
+import 'package:e_commerce/features/splash/presention/views/splash_view.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
   switch (settings.name) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
+    case OnBoardingView.routeName :
+    return MaterialPageRoute(builder: (context) => const OnBoardingView()); 
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
--- lib/features/splash/presention/views/splash_view.dart
+++ lib/features/splash/presention/views/splash_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'views/widgets/splash_view_body.dart';
+import 'widgets/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
