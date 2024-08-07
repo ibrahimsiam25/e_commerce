aed4c857bfd670ccbeac3724403a94fdbbe86219
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-08-07 16:00:52 +0300

    Add Firebase Initialization and Kotlin BOM Dependency
    Initialize Firebase in the main function and add the Kotlin BOM dependency
    to the Android build configuration. This commit ensures that Firebase is
    properly set up for the application, enabling features like authentication,
    cloud storage, and real-time databases. The Kotlin BOM dependency is added
    to manage Kotlin library versions across the project, ensuring compatibility
    and simplifying dependency management.
    
    Additionally, the commit reorganizes the import statements in `main.dart` to
    follow a more logical and readable order, enhancing code maintainability.

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
-import 'package:e_commerce/core/functions/on_generate_routes.dart';
-import 'package:e_commerce/core/utils/app_color.dart';
-import 'package:e_commerce/features/splash/presention/views/splash_view.dart';
-import 'package:e_commerce/generated/l10n.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-
+import 'package:e_commerce/generated/l10n.dart';
+import 'package:e_commerce/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:e_commerce/core/utils/app_color.dart';
 import 'core/services/shared_preferences_singleton.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:e_commerce/core/functions/on_generate_routes.dart';
+import 'package:e_commerce/features/splash/presention/views/splash_view.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SharedPref.init();
   runApp(const ECommerce());
 }
